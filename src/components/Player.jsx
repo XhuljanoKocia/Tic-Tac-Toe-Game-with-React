66b08792277cc62a1b45a8e7c018b075a34f910e
xkocia <xhuljano.kocia@boc-group.com>
2026-07-23 15:59:22 +0200

    User Input & Two-Way-Binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        setIsEditing((editing) => !editing); // => with a function, react schedules a state update which is immediately reflected
+        // setIsEditing(!isEditing); // => schedules a state update to true, but is not immediately reflected
+        // setIsEditing(!isEditing); => in this case this will also be true, because the state update is scheduled and not immediately reflected
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}           
+                {editablePlayerName}           
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
